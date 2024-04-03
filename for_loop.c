9dcf96e1ec4d12919d8f042f2560a6a1a1a19fee
Rohit Kumar Ranjan <rohit124551@gmail.com>
2024-04-04 00:21:21 +0530

    loop practice added 1

---
+++ for_loop.c
@@ -0,0 +1,10 @@
+
+
+
+//print 0 to 10 number in loop
+#include <stdio.h>
+int main(){
+    for(int i =0;i<=10;i=i+1);
+    printf("%d\n", i);
+    return 0;
+}
---
+++ prnt_numbers_frm_0_to_n.c
@@ -0,0 +1,26 @@
+//print numbers from 0 to n, if n is given by user
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//    // int i=0;
+//     for(int i=0;i<=n;i++){
+
+    
+//     printf("%d\n",i);}
+//     return 0;
+
+// }
+
+
+//In while loop
+#include <stdio.h>
+int main(){
+    int i=0;
+    int n;
+    scanf("%d",&n);
+    while(i<=n){
+    printf("%d\n",i);
+    i++;} 
+    return 0;
+}
