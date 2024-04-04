443dbc54fd70963cc8ded028a03bec4a814a3b8a
Rohit Kumar Ranjan <rohit124551@gmail.com>
2024-04-05 00:58:12 +0530

    loop practice added 2

---
+++ funt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void printhello();
+void printgoodbye();
+int main(){
+    printhello();
+    return 0;
+}
+void printhello(){
+    printf("Hello#\n");
+}
+void printgoodbye(){
+    printf("Goodbye\n");
+}
